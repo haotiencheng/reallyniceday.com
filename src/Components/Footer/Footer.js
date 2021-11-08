032705d93ec9cbc18590b33863dd26cf88d4581a
haotiencheng <wolffang871217@gmail.com>
2021-11-08 19:44:41 +0800

    Fix footer responsive issue

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,7 +9,7 @@ import {
 function Footer() {
     return (
         <div className="bg-background fixed grid grid-cols-10 bottom-0 w-full 2xl:text-md xl:text-sm text-xs opacity-80">
-            <div className="items-center justify-self-start md:col-span-7 col-span-9 flex mx-4 md:mx-16 h-8 2xl:h-12">
+            <div className="items-center justify-self-start lg:col-span-7 col-span-9 flex mx-4 md:mx-16 h-8 2xl:h-12">
                 <div>© Cheng Hao Tien 2021｜</div>
                 <div>web developer｜</div>
                 <a href="mailto: haotiencheng1217@gmail.com">
@@ -17,14 +17,14 @@ function Footer() {
                         icon={faEnvelope}
                         className="hidden md:inline-block mr-10 md:mr-2"
                     />
-                    <span className="hidden md:inline-block">
+                    <span className="hidden lg:inline-block">
                         haotiencheng1217@gmail.com｜
                     </span>
                 </a>
                 <a href="tel:+886-905-516-617">
                     <FontAwesomeIcon
                         icon={faPhone}
-                        className="hidden md:inline-block md:mr-2"
+                        className="hidden lg:inline-block md:mr-2"
                     />
                     <span className="hidden md:inline-block">
                         (+886)905-516-617
@@ -32,7 +32,7 @@ function Footer() {
                 </a>
             </div>
             <a
-                className="items-center md:col-span-3 col-span-1 flex justify-self-end mx-4 md:mx-16"
+                className="items-center lg:col-span-3 col-span-1 flex justify-self-end mx-4 md:mx-16"
                 href="#Home"
             >
                 <FontAwesomeIcon icon={faArrowUp} className="mr-2" />
